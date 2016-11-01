f7688a2fabd41f95f6e248c5f4a5bbbb6f9084be
Duc Nguyen <minhducng93@gmail.com>
2016-11-01 20:55:59 +0700

    Modified middleware

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var app = express();
 var PORT = 3000
-var middleware = {
-	requiredAuthentication: function (req, res, next) {
-		console.log('private route hit!');
-		next();
-	},
-	logger: function (req, res, next) {
-		console.log('Request '+ new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-		next();
-	}
-};
+var middleware = require('./middleware.js');
 app.use(middleware.logger);
 
 app.get('/about', middleware.requiredAuthentication, function (req, res) {
